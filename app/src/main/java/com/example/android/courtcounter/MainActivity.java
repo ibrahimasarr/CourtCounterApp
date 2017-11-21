4f283e599020b0ac6c3c58b4f89ed456d2b92c41
Ibrahima SARR <ibrahima.sarr@pulaagu.com>
2017-11-21 09:45:40 +0100

    Updated layout and error correction

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -14,71 +14,168 @@ public class MainActivity extends AppCompatActivity {
      * Tracks Team B score
      */
     int scoreTeamB = 0;
+    /**
+     * Tracks Team A shots
+     */
+    int shotsTeamA = 0;
+    /**
+     * Tracks Team B shots
+     */
+    int shotsTeamB = 0;
+    /**
+     * Tracks Team A shots on target
+     */
+    int shotsOnTargetTeamA = 0;
+    /**
+     * Tracks Team B shots on target
+     */
+    int shotsOnTargetTeamB = 0;
+    /**
+     * Tracks Team A fouls
+     */
+    int foulsTeamA = 0;
+    /**
+     * Tracks Team B fouls
+     */
+    int foulsTeamB = 0;
+    /**
+     * Tracks Team A corners
+     */
+    int cornersTeamA = 0;
+    /**
+     * Tracks Team B corners
+     */
+    int cornersTeamB = 0;
+    /**
+     * Tracks Team A off-sides
+     */
+    int offSidesTeamA = 0;
+    /**
+     * Tracks Team B off-sides
+     */
+    int offSidesTeamB = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(0);
-        displayForTeamB(0);
+        displayScoreTeamA(0);
+        displayScoreTeamB(0);
+        displayShotsTeamA(0);
+        displayShotsTeamB(0);
+        displayOnTargetTeamA(0);
+        displayOnTargetTeamB(0);
+        displayFoulsTeamA(0);
+        displayFoulsTeamB(0);
+        displayCornersTeamA(0);
+        displayCornersTeamB(0);
+        displayOffSidesTeamA(0);
+        displayOffSidesTeamB(0);
     }
 
     /**
      *
-     * Increases Team A score by 1 points.
+     * Increases Team A score by 1 point.
      */
     public void addOneForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
+        displayScoreTeamA(scoreTeamA);
     }
 
     /**
      *
-     * Increases Team A score by 2 points.
+     * Increases Team B score by 1 point.
      */
 
-    public void addTwoForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     *
-     * Increases Team A score by 3 points.
-     */
-
-    public void addThreeForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     *
-     * Increases Team B score by 1 points.
-     */
     public void addOneForTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
+        displayScoreTeamB(scoreTeamB);
     }
 
     /**
      *
-     * Increases Team B score by 2 points.
+     * Increases Team A shots by 1 point.
      */
 
-    public void addTwoForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
+    public void addShotForTeamA(View view) {
+        shotsTeamA = shotsTeamA + 1;
+        displayShotsTeamA(shotsTeamA);
+    }
+
+    /**
+     * Increases Team B shots by 1 point.
+     */
+    public void addShotForTeamB(View view) {
+        shotsTeamB = shotsTeamB + 1;
+        displayShotsTeamB(shotsTeamB);
     }
 
     /**
      *
-     * Increases Team B score by 3 points.
+     * Increases Team A shots on target by 1 point.
      */
 
-    public void addThreeForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
+    public void addOnTargetForTeamA(View view) {
+        shotsOnTargetTeamA = shotsOnTargetTeamA + 1;
+        displayOnTargetTeamA(shotsOnTargetTeamA);
+    }
+
+    /**
+     * Increases Team B shots on target by 1 point.
+     */
+
+    public void addOnTargetForTeamB(View view) {
+        shotsOnTargetTeamB = shotsOnTargetTeamB + 1;
+        displayOnTargetTeamB(shotsOnTargetTeamB);
+    }
+
+    /**
+     * Increases Team A fouls by 1 point.
+     */
+    public void addFoulForTeamA(View view) {
+        foulsTeamA = foulsTeamA + 1;
+        displayFoulsTeamA(foulsTeamA);
+    }
+
+    /**
+     * Increases Team B fouls by 1 point.
+     */
+    public void addFoulForTeamB(View view) {
+        foulsTeamB = foulsTeamB + 1;
+        displayFoulsTeamB(foulsTeamB);
+    }
+
+    /**
+     * Increases Team A corners by 1 point.
+     */
+    public void addCornerForTeamA(View view) {
+        cornersTeamA = cornersTeamA + 1;
+        displayCornersTeamA(cornersTeamA);
+    }
+
+    /**
+     * Increases Team B corners by 1 point.
+     */
+    public void addCornerForTeamB(View view) {
+        cornersTeamB = cornersTeamB + 1;
+        displayCornersTeamB(cornersTeamB);
+    }
+
+    /**
+     * Increases Team A off-sides by 1 point.
+     */
+    public void addOffSideForTeamA(View view) {
+        offSidesTeamA = offSidesTeamA + 1;
+        displayOffSidesTeamA(offSidesTeamA);
+    }
+
+    /**
+     * Increases Team B off-sides by 1 point.
+     */
+    public void addOffSideForTeamB(View view) {
+        offSidesTeamB = offSidesTeamB + 1;
+        displayOffSidesTeamB(offSidesTeamB);
     }
 
     /**
@@ -88,23 +185,124 @@ public class MainActivity extends AppCompatActivity {
     public void resetToZero(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        shotsTeamA = 0;
+        shotsTeamB = 0;
+        shotsOnTargetTeamA = 0;
+        shotsOnTargetTeamB = 0;
+        foulsTeamA = 0;
+        foulsTeamB = 0;
+        cornersTeamA = 0;
+        cornersTeamB = 0;
+        offSidesTeamA = 0;
+        offSidesTeamB = 0;
+        displayScoreTeamA(scoreTeamA);
+        displayScoreTeamB(scoreTeamB);
+        displayShotsTeamA(shotsTeamA);
+        displayShotsTeamB(shotsTeamB);
+        displayOnTargetTeamA(shotsOnTargetTeamA);
+        displayOnTargetTeamB(shotsOnTargetTeamB);
+        displayFoulsTeamA(foulsTeamA);
+        displayFoulsTeamB(foulsTeamB);
+        displayCornersTeamA(cornersTeamA);
+        displayCornersTeamB(cornersTeamB);
+        displayOffSidesTeamA(offSidesTeamA);
+        displayOffSidesTeamB(offSidesTeamB);
     }
 
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+    public void displayScoreTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given score for Team B.
      */
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+    public void displayScoreTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    /**
+     * Displays shots for Team A.
+     */
+    public void displayShotsTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_shots);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays shots for Team B.
+     */
+    public void displayShotsTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_shots);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays shots on target for Team A.
+     */
+    public void displayOnTargetTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_target);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays shots on target for Team A.
+     */
+    public void displayOnTargetTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_target);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays fouls for Team A.
+     */
+    public void displayFoulsTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_fouls);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays fouls for Team B.
+     */
+    public void displayFoulsTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_fouls);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays corners for Team A.
+     */
+    public void displayCornersTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_corners);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays corners for Team B.
+     */
+    public void displayCornersTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_corners);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays off-sides for Team A.
+     */
+    public void displayOffSidesTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_offsides);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays off-sides for Team A.
+     */
+    public void displayOffSidesTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_offsides);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
